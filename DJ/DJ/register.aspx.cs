def7c39367784bbf393d81b21d949a9367fa4748
xixi2020420 <787406408@qq.com>
2020-04-19 16:02:38 +0800

    ...

--- DJ/DJ/register.aspx.cs
+++ DJ/DJ/register.aspx.cs
@@ -3,12 +3,12 @@ using System;
 
 public partial class index : System.Web.UI.Page
 {
-    Manager manager= new Manager();
+    Manager manager = new Manager();
     protected void Page_Load(object sender, EventArgs e)
     {
-        
+
     }
-    
+
     protected void Button2_Click(object sender, EventArgs e)
     {
         Response.Redirect("http://localhost:56935/login.aspx");
@@ -20,23 +20,24 @@ public partial class index : System.Web.UI.Page
         manager.Password = TextBox3.Text;
         manager.Account = new Random().Next(9999, 99999999).ToString();
         if (manager.Tel != string.Empty && manager.Password != string.Empty)
-            //if (BLL.ManagerBusiness.AddManager(manager))
+            if (BLL.ManagerBusiness.AddManager(manager))
             {
-                ////登录成功，数据存入session中
-                //Session["tel"] =manager.Tel;
-                //Session["ID"] = manager.Account;
-                ////Utility.JavaScript.JavaScriptLocationHref("http://localhost:56935/login.aspx", this);
+            //登录成功，数据存入session中
+            Session["tel"] = manager.Tel;
+            Session["ID"] = manager.Account;
+            ////Utility.JavaScript.JavaScriptLocationHref("http://localhost:56935/login.aspx", this);
 
-                //弹窗
-                string scriptstrs = "";
-                scriptstrs += "{swal('这是您的ID！请务必牢记！','13246547987', 'success');}";
-                if (!Page.ClientScript.IsClientScriptBlockRegistered(this.GetType(), "keys"))
-                    Page.ClientScript.RegisterClientScriptBlock(this.GetType(), "keys", scriptstrs, true);
-                Utility.JavaScript.AlertAndRedirect("注册成功，点击确定跳转至登录页", "http://localhost:56935/login.aspx", this);
-            }
+            //弹窗
+            string scriptstrs = "";
+            scriptstrs += "{swal({title: '这是您的ID！',text: " + Button1.Text + ",confirmButtonText: '确认!',confirmButtonColor: '#00b4aa',closeOnConfirm: false,},function(isConfirm) {if (isConfirm){location.href = 'login.aspx'; } });}";
+            if (!Page.ClientScript.IsClientScriptBlockRegistered(this.GetType(), "key"))
+            
+                Page.ClientScript.RegisterClientScriptBlock(this.GetType(), "key", scriptstrs, true);
+
             else
             {
                 Utility.JavaScript.Alert("该账号已存在，请重新填写", this);
             }
+        }
     }
 }
