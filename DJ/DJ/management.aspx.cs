9a27fa082cc6c8c8dab39749a44f7c76f7190184
wycms <124845868@qq.com.com>
2020-05-08 08:55:34 +0800

    new

--- DJ/DJ/management.aspx.cs
+++ DJ/DJ/management.aspx.cs
@@ -1,19 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
+using System.Data.SqlClient;
 using System.Web.UI.WebControls;
 
 public partial class management : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        work work = (work)LoadControl("~/work.ascx");
-        work work1 = (work)LoadControl("~/work.ascx");
-        Panel3.Controls.Add(work);
-        Panel3.Controls.Add(work1);
-        Panel3.Controls.Add(work);
+        SqlDataReader reader = DAL.ManagerDAL.SelectJob("154669");
+        while (reader.Read())
+        {
+            work work = (work)LoadControl("work.ascx");
+            ((Label)work.FindControl("name")).Text = reader.GetString(0);
+
+            ((Label)work.FindControl("renshu")).Text = reader.GetInt32(4).ToString();
+            ((Label)work.FindControl("gongzi1")).Text = reader.GetInt32(5).ToString();
+            Panel3.Controls.Add(work);
+        }
     }
 
     protected void Button发布兼职_Click(object sender, EventArgs e)
--- DJ/DJ/work.ascx.cs
+++ DJ/DJ/work.ascx.cs
@@ -6,18 +6,18 @@ public partial class work : System.Web.UI.UserControl
     protected void Page_Load(object sender, EventArgs e)
     {
        
-        SqlDataReader reader = DAL.ManagerDAL.SelectJob("154669");
-        if (reader.HasRows)
-        {
-            if (reader.Read())
-            {
+        //SqlDataReader reader = DAL.ManagerDAL.SelectJob("154669");
+        //if (reader.HasRows)
+        //{
+        //    if (reader.Read())
+        //    {
                 
-               name.Text = reader.GetString(0);
-                renshu.Text = reader.GetInt32(4).ToString();
-                gongzi1.Text = reader.GetInt32(5).ToString();
+        //       name.Text = reader.GetString(0);
+        //        renshu.Text = reader.GetInt32(4).ToString();
+        //        gongzi1.Text = reader.GetInt32(5).ToString();
 
-            }
-        }
+        //    }
+        //}
 
     }
 
