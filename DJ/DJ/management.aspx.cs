838236bf068e4b81537fccb1ca27b1a2a0604d51
xixi2020420 <787406408@qq.com>
2020-05-08 20:41:14 +0800

    1111

--- DJ/DJ/management.aspx.cs
+++ DJ/DJ/management.aspx.cs
@@ -10,11 +10,13 @@ public partial class management : System.Web.UI.Page
         while (reader.Read())
         {
             work work = (work)LoadControl("work.ascx");
+            
             ((Label)work.FindControl("name")).Text = reader.GetString(0);
             ((Label)work.FindControl("time")).Text = reader.GetString(1);
             ((Label)work.FindControl("where")).Text = reader.GetString(2);
             ((Label)work.FindControl("renshu")).Text = reader.GetInt32(4).ToString();
             ((Label)work.FindControl("gongzi1")).Text = reader.GetInt32(5).ToString();
+            
             user(work, reader);
             //((Label)work.FindControl("name1")).Text = reader.GetString(0);
             //((Label)work.FindControl("time1")).Text = reader.GetString(1);
@@ -22,8 +24,9 @@ public partial class management : System.Web.UI.Page
             //((Label)work.FindControl("jieshao1")).Text = reader.GetString(3);
             //((Label)work.FindControl("renshu1")).Text = reader.GetInt32(4).ToString();
             //((Label)work.FindControl("gongzidaiyu1")).Text = reader.GetInt32(5).ToString();
-
+            
             Panel3.Controls.Add(work);
+            work.wwor();
         }
 
     }
@@ -36,6 +39,7 @@ public partial class management : System.Web.UI.Page
         ((Label)work.FindControl("jieshao1")).Text = reader.GetString(3);
         ((Label)work.FindControl("renshu1")).Text = reader.GetInt32(4).ToString();
         ((Label)work.FindControl("gongzidaiyu1")).Text = reader.GetInt32(5).ToString();
+        
     }
 
     protected void Button发布兼职_Click(object sender, EventArgs e)
--- DJ/DJ/work.ascx.cs
+++ DJ/DJ/work.ascx.cs
@@ -4,7 +4,15 @@ public partial class work : System.Web.UI.UserControl
 { 
     protected void Page_Load(object sender, EventArgs e)
     {
-
+           
     }
     
+    public void wwor()
+    {
+        string scriptstrs = "";
+        scriptstrs += "{ function openDialog() {document.getElementById('light').style.display = 'block';}function closeDialog(){document.getElementById('light').style.display = 'none'; }}";
+        if (!Page.ClientScript.IsClientScriptBlockRegistered(this.GetType(), "key"))
+            Page.ClientScript.RegisterClientScriptBlock(this.GetType(), "key", scriptstrs, true);
+        
+    }
 }
