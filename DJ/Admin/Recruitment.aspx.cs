be939ac62003b0156003ee6f073920fe79ba4a92
xixi2020420 <787406408@qq.com>
2020-05-19 22:20:51 +0800

    cc

--- DJ/Admin/Recruitment.aspx.cs
+++ DJ/Admin/Recruitment.aspx.cs
@@ -15,12 +15,20 @@ public partial class _Default : System.Web.UI.Page
     {
         
     }
+    //筛选前的关闭按钮
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+
+    }
+
+
     //筛选按钮
     protected void select_Click(object sender, EventArgs e)
     {
         ListView1.Visible = false;
         GridView1.Visible = true;
     }
+    //筛选后的关闭按钮
     protected void LinkButton3_Click1(object sender, EventArgs e)
     {
 
