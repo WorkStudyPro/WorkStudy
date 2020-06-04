f806be8d20968872cd2e01f4d95888c29eb93c4e
xixi2020420 <787406408@qq.com>
2020-06-04 14:21:02 +0800

    213

--- DJ/DJ/Admin/Set.aspx.cs
+++ DJ/DJ/Admin/Set.aspx.cs
@@ -4,7 +4,7 @@ public partial class _Default : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        HiddenId.Text = Session["account"].ToString();
     }
   
 }
